6bd15e637e248d2cc06526cd4372e1b683aef04b
Raiyaan Rahman <raiyaan.rahman01@gmail.com>
2022-10-20 18:56:06 -0400

    refactor pawn.py calculate_moves

--- backend/ai_django/app/game_classes/pieces/pawn.py
+++ backend/ai_django/app/game_classes/pieces/pawn.py
@@ -22,35 +22,23 @@ class Pawn(Piece):
                 and board[two_up[0]][two_up[1]] is None):
             self.legal_moves.append(two_up)
 
-        if (in_bounds(left_diag)
-                and board[left_diag[0]][left_diag[1]] is not None
-                and board[left_diag[0]][left_diag[1]].colour != self.colour):
-            self.legal_moves.append(left_diag)
+        for loc in (left_diag, right_diag):
+            if (
+                in_bounds(loc)
+                and board[loc[0]][loc[1]] is not None
+                and board[loc[0]][loc[1]].colour != self.colour
+            ):
+                self.legal_moves.append(loc)
 
-        if (in_bounds(right_diag)
-                and board[right_diag[0]][right_diag[1]] is not None
-                and board[right_diag[0]][right_diag[1]].colour != self.colour):
-            self.legal_moves.append(right_diag)
-
-        # TODO: for en-passent, also need to check if the pawn moved 2 spaces, not 1
-        if (self.row == starting_row + self.direction * 3
-                and in_bounds(left_diag)
-                and board[self.row][self.col - 1] is not None
-                and isinstance(board[self.row][self.col - 1], Pawn)
-                and board[self.row][self.col - 1].colour != self.colour
+            # TODO: for en-passent, also need to check if the pawn moved 2 spaces, not 1
+            if (
+                self.row == starting_row + self.direction * 3
+                and in_bounds(loc)
+                and board[self.row][loc[1]] is not None
+                and isinstance(board[self.row][loc[1]], Pawn)
+                and board[self.row][loc[1]].colour != self.colour
                 and len(move_history) > 0
                 and isinstance(move_history[-1].piece, Pawn)
-                and move_history[-1].piece.id ==
-                board[self.row][self.col - 1].id):
-            self.legal_moves.append(left_diag)
-
-        if (self.row == starting_row + self.direction * 3
-                and in_bounds(right_diag)
-                and board[self.row][self.col + 1] is not None
-                and isinstance(board[self.row][self.col + 1], Pawn)
-                and board[self.row][self.col - 1].colour != self.colour
-                and len(move_history) > 0
-                and isinstance(move_history[-1].piece, Pawn)
-                and move_history[-1].piece.id ==
-                board[self.row][self.col + 1].id):
-            self.legal_moves.append(right_diag)
+                and move_history[-1].piece.id == board[self.row][loc[1]].id
+            ):
+                self.legal_moves.append(loc)
