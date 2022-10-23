4a0f3b06e250fa8ad9a0c70221626d1cd96edf5a
Raiyaan Rahman <raiyaan.rahman01@gmail.com>
2022-10-23 19:05:20 -0400

    rename other_player to opponent

--- backend/ai_django/app/game_classes/game_logic.py
+++ backend/ai_django/app/game_classes/game_logic.py
@@ -7,36 +7,40 @@ PAWNS, KNIGHTS, BISHOPS, ROOKS, QUEENS = PIECE_TYPES
 
 class Logic:
     @staticmethod
-    def calculate_legal_moves(player, other_player, board, move_history):
-        in_check = Logic.in_check(board, player, other_player)
+    def calculate_legal_moves(player, opponent, board, move_history):
+        in_check = Logic.in_check(board, player, opponent)
 
         def helper(piece):
             piece.calculate_moves(board, move_history)
             if in_check:
                 piece.legal_moves = list(filter(
                     lambda move: not Logic.in_check_after_move(
-                        board, other_player, piece.loc, move
+                        board, opponent, piece.loc, move
                     ),
                     piece.legal_moves
                 ))
             else:
-                Logic.validate_moves(board, piece, player, other_player)
+                Logic.validate_moves(board, piece, player, opponent)
 
         helper(player.pieces[KING])
         for piece_type in PIECE_TYPES:
             for piece in player.pieces[piece_type]:
                 helper(piece)
 
+    @staticmethod
+    def make_move(board, player, opponent, move_history, move):
+        pass
+
     # Might want to move this method into Piece to avoid an extra loop when filtering
     # Would have to pass in player
     @staticmethod
-    def in_check_after_move(board, move_history, player, other_player, from_loc, to_loc) -> bool:
+    def in_check_after_move(board, move_history, player, opponent, from_loc, to_loc) -> bool:
         # to avoid deep-copying the board, we can make the changes on the board itself
         # and then reverse the changes
         board_from = board[from_loc[0]][from_loc[1]]
         board_to = board[to_loc[0]][to_loc[1]]
         temp_player = deepcopy(player)
-        temp_opponent = deepcopy(other_player)
+        temp_opponent = deepcopy(opponent)
         copied_piece = temp_player.pieces[board_from.get_type()][board_from.id]
         if board_to is not None and board_to.colour == player.colour:
             raise TypeError('Cannot move to your own piece')
@@ -52,7 +56,7 @@ class Logic:
         Logic.calculate_legal_moves(temp_player, temp_opponent, board, move_history)
         Logic.calculate_legal_moves(temp_opponent, temp_player, board, move_history)
 
-        in_check = Logic.in_check(board, player, other_player)
+        in_check = Logic.in_check(board, player, opponent)
 
         # TODO: uncomment this line: move_history.pop()
         board[from_loc[0]][from_loc[1]] = board_from
@@ -63,11 +67,11 @@ class Logic:
     # filters the pieces moves such that after the move is made, the player's king is not in check
     # pre-condition: the king is not currently in check
     @staticmethod
-    def validate_moves(board, piece, player, other_player):
+    def validate_moves(board, piece, player, opponent):
         if piece.get_type() == KING:
             player.pieces[KING].legal_moves = list(filter(
                 lambda move: not Logic.in_check_after_move(
-                    board, player, other_player, piece.loc, move
+                    board, player, opponent, piece.loc, move
                 ),
                 player.pieces[KING].legal_moves
             ))
@@ -111,19 +115,19 @@ class Logic:
                 validate_in_dir(x_dir, y_dir)
 
     @staticmethod
-    def in_check(board, player, other_player) -> bool:
+    def in_check(board, player, opponent) -> bool:
         # Might want to reimplement by calculating lines from the king (and knight paths)
         # Why? Average branching-factor (i.e. number of legal moves in a position) is 35
         # Number of squares needed using lines is also 35 but that's in the worst case
         # so it could improve performance
         for piece_type in [KNIGHTS, BISHOPS, ROOKS, QUEENS]:
-            for piece in other_player.pieces[piece_type]:
+            for piece in opponent.pieces[piece_type]:
                 if player.pieces[KING].loc in piece.legal_moves:
                     return True
 
         # the other King can't actually put the player in check
         # this is simply to prevent the kings from being adjacent
-        if player.pieces[KING].loc in other_player.pieces[KING].legal_moves:
+        if player.pieces[KING].loc in opponent.pieces[KING].legal_moves:
             return True
 
         left_diag = (player.pieces[KING].row + 1 * player.direction, player.pieces[KING].col - 1)
