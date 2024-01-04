568a854e0100b927e36393165ee3fc46c4f3a4e1
Raiyaan Rahman <raiyaan.rahman01@gmail.com>
2024-01-04 00:50:21 -0500

    remove databases from settings.py

--- backend/ai_django/ai_django/settings.py
+++ backend/ai_django/ai_django/settings.py
@@ -78,15 +78,15 @@ WSGI_APPLICATION = 'ai_django.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'ai-carlsen-db', 
-        'USER': 'postgres', 
-        'HOST': '127.0.0.1', 
-        'PORT': '5432',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'ai-carlsen-db', 
+#         'USER': 'postgres', 
+#         'HOST': '127.0.0.1', 
+#         'PORT': '5432',
+#     }
+# }
 
 
 # Password validation
